abfc03e08bf8dd301404d1228ec6e152947216ed
alejandrossm <alejandrossm@gmail.com>
2023-05-04 17:53:38 -0400

    Modelo persona ok

--- appcongit/models.py
+++ appcongit/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Persona(models.Model):
+    rut=models.TextField(max_length=10,primary_key=True)
+    nombre=models.TextField(max_length=50)
+    apellido=models.TextField(max_length=50)
+    
+    def __str__(self):
+        return f"{self.rut}-{self.nombre} {self.apellido}"
+
